6fa2cad50f801614d96578ef75038027a082d86b
amineohn <amineprojet7@gmail.com>
2022-01-31 09:32:57 +0100

    feat(animation): cool animation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,13 @@
 export default function Index() {
   return (
     <div className="flex flex-col justify-center items-center h-screen select-none lg:mx-0 mx-2">
-      <div className="inline-flex justify-center items-center">
+      <div className="inline-flex justify-center items-center slide-in-elliptic-top-fwd">
         <h2 className="text-2xl lg:text-7xl text-white font-bold drop-shadow-lg">
           RemixJS Experimentation
         </h2>
         <span className="text-lg lg:text-6xl">&#128540;</span>
       </div>
-      <div className="text-xl text-white">
+      <div className="text-xl text-white slide-in-elliptic-top-fwd">
         <p>That's cool &#129322;</p>
       </div>
     </div>
